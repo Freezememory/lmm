bea1cf3add153d2d0056097f4f925825cf7ea412
Wanglj <1152589335@qq.com>
2023-03-13 23:10:45 +0800

    基本数据结构学习

---
+++ lmm-biz/lmm-portal/src/main/java/com/wanglj/lmm/portal/service/DataStructureService.java
@@ -0,0 +1,21 @@
+package com.wanglj.lmm.portal.service;
+
+import java.util.Stack;
+
+/**
+ * @author Wanglj
+ * @date 2023/3/8 21:34
+ * @desc 数据结构service
+ */
+public class DataStructureService {
+
+
+    public static void stackDemo() {
+        Stack<Integer> stack = new Stack<>();
+        stack.add(123);
+        stack.add(123);
+        stack.add(123);
+    }
+
+
+}
